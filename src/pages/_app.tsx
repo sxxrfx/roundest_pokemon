4ed631c97fe0f5c835c2c1655ae5ab2f6eca751a
Sagar Negi <sxxrfx@gmail.com>
2023-03-27 21:42:42 +0530

    Restarting the projec

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../styles/globals.css'
+import type { AppType } from 'next/app';
+import { trpc } from '../utils/trpc';
 
-import type { AppProps } from 'next/app'
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
-}
-export default MyApp;
+export default trpc.withTRPC(MyApp);
